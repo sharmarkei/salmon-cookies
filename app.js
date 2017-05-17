4440b08da72034244154571bfe64f059092078ed
sharmarke1 <sharmarkeibrahim@Sharmarke-Ibrahims-MacBook-Pro.local>
2017-05-17 10:07:06 -0700

    started working on table

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ function Store(name,minCust,maxCust,avgCookiesPerHr) {
   this.totalCookiesSoldArray = 0;
   this.populateCookiesPerHrArray();
   this.totalCookiesPerStoreDaily();
+  this.listThings();
 }
 
 // Populate array of avg customers per hour
@@ -57,25 +58,32 @@ var storeArr = [firstPike, seaAirport, seaCenter, capHill, alki];
 
 
 
-var table = document.getElementById('shell');
-var data = [];
+listStoreInTable = function() {
+  var container = document.createElement('table');
+  container.innerHTML = '<thead></thead>';
+  document.body.appendChild(container);
 
-for (var i=0, i < storeArr.length; i++) {
-  data.push (
-    // '<td>' + storeArr[i]. + '</td>' +
-    '<td>' + storeArr[i].name + '</td>' +
-    '<td>' + storeArr[i].minCust + '</td>' +
-    '<td>' + storeArr[i].maxCust + '</td>' +
-    '<td>' + storeArr[i].avgCookiesPerHr + '</td>'
-  )
-}
-var new_row;
+  var list = document.createElement('thead');
+  var listArr = [];
 
-for (var j = 0; j < data.length; j++) {
-  new_row = document.createElement('tr');
-  new_row.innerHTML = data[j];
-  table.appendChild(new_row);
-}
+  for (var i = 0; i < hours.length; i++) {
+
+    if (i < 5) {
+      listArr.push('<td>' + hours[i] + ': ' + this.cookiesPerHrArray[i] + ' cookies</td>');
+    } else {
+      listArr.push('<td>' + hours[i] + ': ' + this.cookiesPerHrArray[i] + ' cookies</td>');
+
+    }
+  }
+
+  listArr.push('<td>Total: ' + this.totalCooks + ' cookies</td>');
+
+  var fullString = listArr.join('');
+  list.innerHTML = fullString;
+  document.body.appendChild(list);
+};
+
+
 
 //
 //   // Populate array of avg customers per hour
